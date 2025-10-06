ee40197e4e6fdbcc69497ee0b1d4c49fc8f3eb2a
Leon <leonftipton@gmail.com>
2025-10-06 15:57:29 +0000

    started developing wordle

--- portfolio/wordle/src/main/kotlin/Wordle.kt
+++ portfolio/wordle/src/main/kotlin/Wordle.kt
@@ -1,1 +1,35 @@
 // Implement the six required functions here
+import java.io.File
+import kotlin.random.Random
+
+fun isValid(word: String): Boolean = word.length == 5
+
+fun readWordList(filename: String): MutableList<String> = File("words.txt").useLines { it.toMutableList<String>() }
+
+fun pickRandomWord(words: MutableList<String>): String {
+    val index = Random.nextInt(0, words.size)
+    val word = words[index]
+    words.removeAt(index)
+    return word
+}
+
+fun obtainGuess(attempt: Int): String {
+    println("Attempt $attempt")
+    print("Enter your guess: ")
+    return readln()
+}
+
+fun evaluateGuess(guess: String, target: String): list<Int> {
+    val ret = intListOf (5)
+    for (i in 0..4) {
+        ret[i] = 1 if guess[i] == target[i] else 0
+    }
+    return ret
+}
+
+fun displayGuess(guess: String, matches: List<Int>) {
+    var out: String
+    for (i in 0..4) {
+        out.append(guess[i]) if matches[i] == 1 else "?"
+    }
+}
