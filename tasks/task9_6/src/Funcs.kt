5d280fcc54b58901c1101bf83abc697f9ab5055a
Leon <leonftipton@gmail.com>
2025-10-16 14:13:07 +0000

    Update funcs.kt, Main.kt

--- tasks/task9_6/src/Funcs.kt
+++ tasks/task9_6/src/Funcs.kt
@@ -1,5 +1,24 @@
 // Task 9.6: functions for dataset variance calculation
+import kotlin.math.pow
+import java.io.File
 
 // Add variance() function here
+fun variance(dataset: List<Double>): Double {
+    require(dataset.size > 1) { "not enough data" }
+    
+    val mean = dataset.average()
+    println(mean)
+    var summation: Double = 0.0
 
+    for (dp in dataset) {
+        summation += (dp - mean).pow(2)
+    }
+
+    return summation / (dataset.size - 1)
+}
 // Add readData() function here
+fun readData(filename: String) = buildList {
+    File(filename).forEachLine {
+        add(it.toDouble())
+    }
+}
--- tasks/task9_6/src/Main.kt
+++ tasks/task9_6/src/Main.kt
@@ -1,7 +1,30 @@
 // Task 9.6: application to compute dataset variance
 
 import kotlin.system.exitProcess
+import java.io.FileNotFoundException
 
 fun main(args: Array<String>) {
     // Implement main program here
+    if (args.size != 1) {
+        println("File not supplied")
+        exitProcess(1)
+    }
+    
+    var dataset = listOf<Double>()
+
+    try {
+        dataset = readData(args[0])
+    } catch (error: FileNotFoundException) {
+        println("Error: file not found")
+        exitProcess(1)
+    } catch (error: Exception) {
+        println("Error: file parse failed")
+        exitProcess(1)
+    }
+
+    val variance = variance(dataset)
+
+    println("The variance of ${dataset} is ${variance}")
+
+    exitProcess(0)
 }
