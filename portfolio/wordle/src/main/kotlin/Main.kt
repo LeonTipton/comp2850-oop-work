ab46f0473f1e2f701fabdce13074181c326642a1
Leon <leonftipton@gmail.com>
2025-10-20 14:42:52 +0000

    Changed allowed guesses to 6 and targetEval to 2

--- portfolio/wordle/src/main/kotlin/Main.kt
+++ portfolio/wordle/src/main/kotlin/Main.kt
@@ -1,16 +1,15 @@
 fun main() {
     val words = readWordList("./data/words.txt")
     var play: Boolean = true
-    val targetEval = List<Int> (5) { 1 }
+    val targetEval = List<Int> (5) { 2 }
     while (play) {
         val target = pickRandomWord(words)
-        println(target)
-        for (i in 1..10) {
+        for (i in 1..6) {
             val guess = obtainGuess(i)
             val eval = if (isValid(guess)) evaluateGuess(guess, target) else listOf<Int>()
             if (eval == listOf<Int>()) {
                 println("Invalid guess")
-                if (i == 10) {
+                if (i == 6) {
                     println("""
                     |You haven't correctly guessed the word.
                     |It was ${target}.
@@ -27,7 +26,7 @@ fun main() {
                 break
             }
             displayGuess(guess, eval)
-            if (i == 10) {
+            if (i == 6) {
                 println("""
                 |You haven't correctly guessed the word.
                 |It was ${target}.
