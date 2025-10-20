62cffd16991f2f24d2d43ce627274d1659173e0e
Leon <leonftipton@gmail.com>
2025-10-20 15:02:00 +0000

    update point.py

--- tasks/task12_1/point.py
+++ tasks/task12_1/point.py
@@ -1,1 +1,25 @@
+import math
+from math import pow
+class Point:
+    def __init__(self, x, y):
+        self.x = x
+        self.y = y
+    
+    def distance(self):
+        return math.sqrt(pow(self.x, 2) + pow(self.y, 2))
+    
+    def distanceTo(self, p):
+        return math.sqrt(pow(self.x - p.x, 2) + pow(self.y - p.y,2))
 
+def main():
+    p = Point(4.5, 7.0)
+
+    x = float(input("Enter x-coordinate: "))
+    y = float(input("Enter x-coordinate: "))
+    
+    uPoint = Point(x, y)
+
+    print(f"Distance from origin {uPoint.distance()}")
+    print(f"Distance from (4.5, 7.0) {round(uPoint.distanceTo(p), 3)}")
+
+if __name__ == "__main__": main()
