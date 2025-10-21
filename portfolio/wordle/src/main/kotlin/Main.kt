3e996605ad0bdc3ec00c0b3301d30be1ce22f558
Leon <leonftipton@gmail.com>
2025-10-21 13:10:38 +0000

    update to display characters remaining

--- portfolio/wordle/src/main/kotlin/Main.kt
+++ portfolio/wordle/src/main/kotlin/Main.kt
@@ -1,25 +1,31 @@
-const val attemptLimit = 6
+const val ATTEMPT_LIMIT = 6
 
 fun main() {
     val words = readWordList("./data/words.txt")
     var play: Boolean = true
     
-    val targetEval = List<Int> (5) { 2 }
+    val targetEval = List<Int>(5) { 2 }
     while (play) {
+        var charactersLeft = "ABCDEFGHIJKLMNOPQRSTUVWXYZ".toList()
         val target = pickRandomWord(words)
-        for (i in 1..attemptLimit) {
+        for (i in 1..ATTEMPT_LIMIT) {
             val guess = obtainGuess(i)
             val eval = evaluateGuess(guess, target)
                        
             if (eval == targetEval) {
                 println("""
                 |Congratulations!!
+                |The word was ${"\u001b[32m" + target + "\u001b[0m"}
                 |You got the correct answer in ${i} attempts.
                 """.trimMargin())
                 break
             }
+
             displayGuess(guess, eval)
-            if (i == attemptLimit) {
+            charactersLeft = remainingChars(guess, eval, charactersLeft)
+            displayChars(charactersLeft, target, eval)
+
+            if (i == ATTEMPT_LIMIT) {
                 println("""
                 |You haven't correctly guessed the word.
                 |It was ${target}.
