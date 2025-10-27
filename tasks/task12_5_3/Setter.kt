ffe08178edcb4a12e04e58dbfef1917885773872
Leon <leonftipton@gmail.com>
2025-10-27 18:29:17 +0000

    update 12.5.3

--- tasks/task12_5_3/Setter.kt
+++ tasks/task12_5_3/Setter.kt
@@ -2,6 +2,21 @@
 
 import java.time.LocalDate
 
-class Person(var name: String, val birth: LocalDate) {
+class Person(_name: String, val birth: LocalDate) {
+    constructor(_name: String, birth: String):  this(_name, LocalDate.parse(birth))
+
     var isMarried = false
+
+    var name = _name
+        set(value) {
+            require(value.isNotBlank()) { "Name cannot be blank" }
+            field = value
+        }
+}
+
+fun main() {
+    val nm = readln()
+    val person = Person(nm, "1981-12-08")
+    person.name = ""
+    println(person.name)
 }
