5b0c405d821005a43ea0b57024788f2a91a59fbe
Leon <leonftipton@gmail.com>
2025-10-27 18:37:14 +0000

    update 12.5.4

--- tasks/task12_5_4/Age.kt
+++ tasks/task12_5_4/Age.kt
@@ -1,7 +1,15 @@
 // Task 12.5.4: class with a computed property
 
 import java.time.LocalDate
+import java.time.temporal.ChronoUnit.YEARS
 
 class Person(var name: String, val birth: LocalDate) {
+    constructor(_name: String, birth: String):  this(_name, LocalDate.parse(birth))
     var isMarried = false
+    val age get() = YEARS.between(birth, LocalDate.now()).toInt()
+}
+
+fun main() {
+    val p = Person("name", "1974-01-08")
+    println(p.age)
 }
