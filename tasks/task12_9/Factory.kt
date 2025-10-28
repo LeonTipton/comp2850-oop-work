122c37c11c834d147e9ec0cb57717d69af71fb7e
Leon <leonftipton@gmail.com>
2025-10-28 14:09:57 +0000

    update 12.9

--- tasks/task12_9/Factory.kt
+++ tasks/task12_9/Factory.kt
@@ -2,4 +2,21 @@
 
 import java.time.LocalDate
 
-class Person(val name: String, val birth: LocalDate)
+class Person private constructor(val name: String, val birth: LocalDate) {
+    companion object Factory {
+        // constructor(name: String, birth: String):  this(name, LocalDate.parse(birth))
+        private val names = mutableSetOf<String>()
+
+        fun create(name:String, birth: LocalDate): Person {
+            require(name.isNotBlank()) { "Name cannot be blank" }
+            require(name !in names) { "Name '$name' is already taken" }
+            names.add(name)
+            return Person(name, birth)
+        }
+    }
+}
+
+fun main() {
+    val person1 = Person.create("alice", LocalDate.parse("1918-12-07"))
+    val person2 = Person.create("alice", LocalDate.parse("1918-12-05"))
+}
