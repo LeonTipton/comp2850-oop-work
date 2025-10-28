80c2f7d157e932c51b7770933f240cd58eed1441
Leon <leonftipton@gmail.com>
2025-10-28 13:57:52 +0000

    update 12.8

--- tasks/task12_8_2/src/Card.kt
+++ tasks/task12_8_2/src/Card.kt
@@ -1,5 +1,36 @@
 // Task 12.8.2: class to represent playing cards
 class Card(val rank: Rank, val suit: Suit) {
+    
+    enum class Rank(val num: Char) {
+        Ace('A'),
+        Two('2'),
+        Three('3'),
+        Four('4'),
+        Five('5'),
+        Six('6'),
+        Seven('7'),
+        Eight('8'),
+        Nine('9'),
+        Ten('T'),
+        Jack('J'),
+        Queen('Q'),
+        King('K'),;
+
+        override fun toString(): String = "$num"
+    }
+
+    enum class Suit(val symbol: Char) {
+        Clubs('♣'),
+        Diamonds('♦'),
+        Hearts('♥'),
+        Spades('♠');
+
+        val plainSymbol get() = name[0]
+
+        override fun toString() = "$symbol"
+    }
+
     val fullName get() = "${rank.name} of ${suit.name}"
     override fun toString(): String = "${rank}${suit}"
+
 }
--- tasks/task12_8_2/src/Main.kt
+++ tasks/task12_8_2/src/Main.kt
@@ -1,4 +1,6 @@
 // Task 12.8.2: program to manipulate playing cards
+import Card.Rank
+import Card.Suit
 
 fun main() {
     val deck = mutableListOf<Card>()
