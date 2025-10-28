2e50c8c6abe868156c532be3847ee6b5f2f430cb
Leon <leonftipton@gmail.com>
2025-10-28 14:46:31 +0000

    update 13.3

--- tasks/task13_3/src/main/kotlin/Money.kt
+++ tasks/task13_3/src/main/kotlin/Money.kt
@@ -1,3 +1,13 @@
 // Task 13.3
 
-// Implement your Money class here
+data class Money(val euros: Int, val cents: Int) {
+    init {
+        require(euros >= 0) { "Euros cannot be less than zero" }
+        require(cents in 0..99) { "Cents must be between 0 and 99" }
+    }
+
+    operator fun plus(other: Money) = if (cents + other.cents < 100) 
+        Money(euros + other.euros, cents + other.cents) 
+    else 
+        Money(euros + other.euros + 1, cents + other.cents - 100)
+}
--- tasks/task13_3/src/test/kotlin/MoneyTest.kt
+++ tasks/task13_3/src/test/kotlin/MoneyTest.kt
@@ -1,11 +1,47 @@
 // Task 13.3
 
 import io.kotest.assertions.throwables.shouldThrow
+import io.kotest.assertions.throwables.shouldThrowAny
 import io.kotest.assertions.withClue
 import io.kotest.core.spec.style.StringSpec
 import io.kotest.matchers.shouldBe
 
 @Suppress("unused")
 class MoneyTest : StringSpec({
-    // write your unit tests here
+    "Can create a Money object" {
+        val m = Money(1, 50)
+        withClue("euros") { m.euros shouldBe 1 }
+        withClue("cents") { m.cents shouldBe 50 }
+    }
+
+    "Can create a Money object with different parameters" {
+        val m = Money(2, 99)
+        withClue("euros") { m.euros shouldBe 2 }
+        withClue("cents") { m.cents shouldBe 99 }
+    }
+    "Cannot create a money object with an invalid number of euros" {
+        shouldThrow<IllegalArgumentException> { Money(-1, 50) }
+    }
+    "Cannot create a money object with an invalid number of cents" {
+        shouldThrow<IllegalArgumentException> { Money(1, -50) }
+    }
+    "Cannot create a money object with an invalid number of euros & cents" {
+        shouldThrow<IllegalArgumentException> { Money(-1, -50) }
+    }
+    "Cannot create a money object with cents > 99" {
+        shouldThrow<IllegalArgumentException> { Money(-2, 120) }
+    }
+    "€1.50 + €1.00 is €2.50" {
+        Money(1, 50) + Money(1, 0) shouldBe Money(2, 50) 
+    }
+    "€1.50 + €2.00 is €3.50" {
+        Money(1, 50) + Money(2, 0) shouldBe Money(3, 50)
+    }
+    "€1.50 + €0.01 is €1.51" {
+        Money(1, 50) + Money(0, 1) shouldBe Money(1, 51)
+    }
+    "€2.99 + €0.01 is €3.00" {
+        Money(2, 99) + Money(0, 1) shouldBe Money(3, 0)
+    }
+
 })
